9617d956a3597276d16f05295f548611c12840f7
Rachael <rachael92njoki@gmail.com>
2024-01-12 11:47:53 +0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Flatdango
 mini app
+Discription
+This is an application that allows a user to purchase a movie ticket from Flatiron Movie Theater.
+
+Built with
+HTML
+CSS
+JavaScript
+Bootstrap
+Running the application
+Click on any movie of choice such as: "Village of the Giants"
+On the right panel the movie opens.
+The right panel indicates the capacity of the theater, the tickets bought and the remaining tickets.
+Click on the blue button written "get ticket".
+If you buy one ticket, the number of available tickets also decreases.
+Once all the tickets have been bought, the application indates that there are no available tickets.
+License
+MIT
+
+Author
+Rachael Njoki
---
+++ index.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // This is DOM manipulation 
+    const moviesListContainer = document.querySelector("#moviesList");
+    const movieDetailsContainer = document.querySelector("#movieDetails");
+  
+    // Obtaining the film data. 
+    fetch("https://flatdango.vercel.app/db.json")
+      .then((res) => res.json())
+      .then((data) => {
+        displayMoviesList(data.films);
+      });
+  
+    //   Indicating the movie titles. 
+    function displayMoviesList(data) {
+      data.map((movie) => {
+        const markUp = `<li class="list-group-item" id="movieList">${movie.title}</li>`;
+  
+        // appending the titles of the movie.
+        moviesListContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+        // Showing the list of the movies on the user's landing page. 
+        displayMovieDetails(movie);
+  
+        // Indicating the details of the movie on the right panel. 
+        const movieList = document.querySelector("#movieList");
+        movieList.addEventListener("click", () => {
+          displayMovieDetails(movie);
+        });
+      });
+    }
+  
+    // Showing the details of the movie.
+    function displayMovieDetails(movie) {
+      const markUp = `<img src="${movie.poster}" alt="">
+    <div>
+    <h2 id="movieTitle" class= "customtitles">${movie.title}</h2>
+    <p id="description">${movie.description}</p>
+    <p id="runtime">Length: <span>${movie.runtime} Minutes</span></p>
+    <p id="showtime">Show Time: <span>${movie.showtime}</span></p>
+    <p id="capacity">Theater capacity: <span>${movie.capacity}</span></p>
+    <p id="ticketssold">Tickets sold: <span>${movie.tickets_sold}</span></p>
+    <p id="availableTickets">Available tickets: <span>${
+      movie.capacity - movie.tickets_sold
+    }</span></p>
+    <button class="custombtn" id="buyTicket">Get ticket</button>
+   </div>`;
+  
+      movieDetailsContainer.innerHTML = "";
+      movieDetailsContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+      const btn = movieDetailsContainer.querySelector("button");
+      btn.addEventListener("click", () => {
+        buyTicket(movie);
+      });
+    }
+  
+    // To purchase the ticket 
+    function buyTicket(movie) {
+      const ticketsSold = document.querySelector("#ticketssold");
+      let remainingTickets = movie.capacity - movie.tickets_sold;
+      const btn = movieDetailsContainer.querySelector("button");
+      const availableTickets = document.querySelector("#availableTickets");
+  
+      if (remainingTickets > 0) {
+        movie.tickets_sold++;
+        remainingTickets--;
+        btn.innerHTML = "Get ticket";
+      } else {
+        btn.innerHTML = "Ticket depleted";
+        btn.classList.add("soldOut");
+      }
+      ticketsSold.innerHTML = `Tickets sold: <span>${movie.tickets_sold}</span>`;
+      availableTickets.innerHTML = `Available tickets: <span>${remainingTickets}</span>`;
+    }
+  });
